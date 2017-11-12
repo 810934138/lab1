e5733e3c7160378402f0d2fcc9e4146ec337f5c5
810934138 <810934138@qq.com>
2017-11-12 21:55:24 +0800

    lab6w

--- src/AllTests.java
+++ src/AllTests.java
@@ -3,7 +3,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ tst1.class, tst2.class, tst3.class, tst4.class, tst5.class,tst6.class})
+@SuiteClasses({ tst1.class, tst2.class, tst3.class, tst4.class, tst5.class,tst6.class,tsw1.class,tsw2.class,tsw3.class,tsw4.class,tsw5.class,tsw6.class})
 public class AllTests {
 
 }
---
+++ src/tsw6.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class tsw6 {
+
+	@Test
+	public void testCalcShortestPath1() {
+		org.junit.Assert.assertEquals("4",Directed_graph.queryBridgeWords("word","in"));
+	}
+	@BeforeClass  
+    public static void testBeforeClass() throws IOException{//必须为静态方法  
+        Directed_graph.CreateGraph("D:\\words(1).txt");  
+    }
+	
+	@Before  
+    public void testBefore(){  
+        System.out.println("@Before");  
+    }     
+    @After  
+    public void testAfter(){  
+        System.out.println("@After");  
+    }  
+    @AfterClass  
+    public static void testAfterClass(){//必须为静态方法  
+        System.out.println("@AfterClass");  
+    }  
+}
